f222b754e58d5c6e33d3ae9751b4b12269d009bc
keshav514 <keshav@Keshavs-Air.Dlink>
2017-03-19 19:54:28 +0530

    Creation of Factorial through for loop and product method

---
+++ myFacto.m
@@ -0,0 +1,14 @@
+% Calculate factorial of n
+n = 6;
+factValue = 1;
+
+for i = 1:6
+factValue = factValue*i;
+end
+
+% One more method or can be used without for loop
+
+m = 6;
+ factValuem = prod(1:m);
+ 
+
